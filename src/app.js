833947b31c6f818ee1b35485e8e3ee3f223724e5
Mohamed Ben Chamakh <mohamed.ben.chamakh98@gmail.com>
2024-02-26 22:40:55 +0100

    proxy

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 //CROSS-ORIGIN
 const corsOptions = {
-    origin: ['https://flooka-tv.vercel.app','https://*.shahid.net/'], // Pass the allowed origins as an array
+    origin: ['https://flooka-tv.vercel.app','https://shls-m6-france-prod-dub.shahid.net/out/v1/c8a9f6e000cd4ebaa4d2fc7d18c15988/index_3.m3u8','https://shls-spacetoon-prod-dub.shahid.net/out/v1/6240b773a3f34cca95d119f9e76aec02/index.m3u8'], // Pass the allowed origins as an array
     methods: ['GET', 'POST'], // Optionally, specify allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Optionally, specify allowed headers
     credentials: true, // Optionally, enable credentials
