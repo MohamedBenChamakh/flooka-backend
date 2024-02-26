c25dc2e099bb7da2d88841c3e895ffd124d764b6
Mohamed Ben Chamakh <mohamed.ben.chamakh98@gmail.com>
2024-02-26 20:05:09 +0100

    proxy

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 //CROSS-ORIGIN
 const corsOptions = {
-    origin: '*', // Pass the allowed origins as an array
+    origin: 'https://flooka-tv.vercel.app', // Pass the allowed origins as an array
     methods: ['GET', 'POST'], // Optionally, specify allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Optionally, specify allowed headers
     credentials: true, // Optionally, enable credentials
