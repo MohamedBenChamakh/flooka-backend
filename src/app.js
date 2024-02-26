c97234ec4b542e3455956df01664c91aa0cc69a7
Mohamed Ben Chamakh <mohamed.ben.chamakh98@gmail.com>
2024-02-26 15:28:20 +0100

    gitignore

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const channelRoutes = require("./routes/channel");
 
 //CROSS-ORIGIN
 app.get((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", process.env.FRONT_URI);
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
