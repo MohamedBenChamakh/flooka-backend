4e17b01e22ff1f2f451b7ade1853847a56254601
Mohamed Ben Chamakh <mohamed.ben.chamakh98@gmail.com>
2024-02-26 16:19:49 +0100

    proxy

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const http = require('http');
 const app = require('./src/app');
 const server = http.createServer(app);
-//const cors = require('cors');
-//app.use(cors());
+const cors = require('cors');
+
+app.use(cors());
+//CROSS-ORIGIN
+app.get((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+});
+
 app.set('view engine', 'ejs');
 
 const normalizePort = val => {
--- src/app.js
+++ src/app.js
@@ -8,19 +8,7 @@ proxyMiddleware(app);
 
 const channelRoutes = require("./routes/channel");
 
-//CROSS-ORIGIN
-app.get((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    );
-    next();
-});
+
 
 app.use(bodyParser.json());
 mongodb.connectToDatabase();
